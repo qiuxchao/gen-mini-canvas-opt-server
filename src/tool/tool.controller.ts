aa27ea8cdf0664884c7ee621b239aa8cf1bb38a3
qiuxchao <2915241235@qq.com>
2023-06-13 18:12:11 +0800

    🧩 update: 上传文件

--- src/tool/tool.controller.ts
+++ src/tool/tool.controller.ts
@@ -15,8 +15,6 @@ export class ToolController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log('file: ', file); // 打印文件信息
-    // 处理文件流
     const result = await this.toolService.uploadFile(file);
     return result;
   }
