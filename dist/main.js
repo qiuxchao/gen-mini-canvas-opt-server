9b1c4da750e655ca719212b8de9feada1bad72dc
qiuxchao <2915241235@qq.com>
2023-06-29 15:58:15 +0800

    🔧 build

--- dist/main.js
+++ dist/main.js
@@ -6,10 +6,14 @@ const response_1 = require("./common/interceptors/response");
 const catchFilters_1 = require("./common/interceptors/catchFilters");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: 'https://gen-canvas-opt-code.vercel.app',
+        methods: ['GET', 'POST'],
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma, Authorization',
+    });
     app.useGlobalInterceptors(new response_1.Response());
     app.useGlobalFilters(new catchFilters_1.HttpFilter());
-    await app.listen(6923);
+    await app.listen(process.env.PORT || 6923);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
