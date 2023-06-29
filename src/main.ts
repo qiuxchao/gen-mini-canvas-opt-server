bb8b72f02c8816de4bc301ecfb1b941dfcbb3022
qiuxchao <2915241235@qq.com>
2023-06-29 13:29:26 +0800

    🧩 update: 跨域处理

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { Response } from './common/interceptors/response';
 import { HttpFilter } from './common/interceptors/catchFilters';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // 跨域
-  app.enableCors();
+  // 处理跨域
+  app.enableCors({
+    origin: [/gen-canvas-opt-code\.vercel\.app$/],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   // 响应包装拦截器
   app.useGlobalInterceptors(new Response());
   // 异常过滤器
