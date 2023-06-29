33a86061ae52978558965ca6ab60732d10073589
qiuxchao <2915241235@qq.com>
2023-06-29 10:54:46 +0800

    🐳 chore: @vercel/node

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { Response } from './common/interceptors/response';
 import { HttpFilter } from './common/interceptors/catchFilters';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 跨域
+  app.enableCors();
   // 响应包装拦截器
   app.useGlobalInterceptors(new Response());
   // 异常过滤器
