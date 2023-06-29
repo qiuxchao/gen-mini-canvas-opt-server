46b48b39d67fa6ef3457ae209f2ce2ffd87685ac
qiuxchao <2915241235@qq.com>
2023-06-29 16:24:00 +0800

    🔧 build

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from 'src/common/decorators/public.decorator';
 import { SignInDto } from './dto/sign-in.dto';
@@ -16,6 +23,14 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async signIn(@Body(ValidationPipe) signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  /** 登录 */
+  @Public()
+  @Get('sign-in')
+  @HttpCode(HttpStatus.OK)
+  async signin() {
+    return true;
   }
 
   /** 注册 */
